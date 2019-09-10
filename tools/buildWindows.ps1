c459b2ccab58d065b7ac656ef291de75ae89ae83
Thiago Lugli <thiago.lugli@ifood.com.br>
2019-09-10 11:17:31 -0300

    Single node version build, with pre-gyp for all

--- tools/buildWindows.ps1
+++ tools/buildWindows.ps1
@@ -1,20 +1,18 @@
+# Build Win32 64bit
+node-pre-gyp configure --target=$env:nodejs_version
+node-pre-gyp build package --target=$env:nodejs_version --target_arch=x64
 
-if ([int]$env:nodejs_version -gt 6) {
-  # Build Win32 64bit
-  node-pre-gyp configure
-  node-pre-gyp build package
+# Build Win32 32bit
+node-pre-gyp configure --target=$env:nodejs_version
+node-pre-gyp build package --target=$env:nodejs_version --target_arch=ia32
 
-  # Build Win32 32bit
-  node-pre-gyp configure
-  node-pre-gyp build package --target_arch=ia32
-
-  if ($env:build_electron -ne "true") {
-    echo "Skipping Electron Build as flag not set"
-    return
-  }
-
-  # Build Electron Versions
-  ./tools/buildElectronWindows.ps1 1.2.8
-  ./tools/buildElectronWindows.ps1 1.3.8
-  ./tools/buildElectronWindows.ps1 1.4.6
+if ($env:build_electron -ne "true") {
+  echo "Skipping Electron Build as flag not set"
+  return
 }
+
+# Build Electron Versions
+./tools/buildElectronWindows.ps1 1.2.8
+./tools/buildElectronWindows.ps1 1.3.8
+./tools/buildElectronWindows.ps1 1.4.6
+./tools/buildElectronWindows.ps1 1.7.12
