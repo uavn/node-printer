b98836b9b6ebf8bfeb7243e33506b603a8d073f1
Thiago Lugli <thiago.lugli@ifood.com.br>
2019-09-10 15:45:34 -0300

    Trying to build electron version

---
+++ tools/buildElectronLinux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VERSION=$1
+
+# # Build Electron Linux 64bit
+node-pre-gyp configure --target=$VERSION --arch=x64 --dist-url=https://electronjs.org/headers --module_name=node_printer --module_path=../lib/
+node-pre-gyp build package --runtime=electron --target=$VERSION --target_arch=x64 --build-from-source
+
+# #Build Electron Linux 32bit
+# node-gyp configure --target=$VERSION --arch=ia32 --dist-url=https://atom.io/download/electron --module_name=node_printer --module_path=../lib/
+# node-gyp build --target=$VERSION --arch=ia32 --dist-url=https://atom.io/download/electron --module_name=node_printer --module_path=../lib/
+# node-pre-gyp package --runtime=electron --target=$VERSION --target_arch=ia32
--- tools/buildLinux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build Linux 64bit
-node-pre-gyp configure --target=$NODE_VERSION
-node-pre-gyp build package --target=$NODE_VERSION --target_arch=x64
-
-# Build Linux 32bit
-node-pre-gyp configure --target=$NODE_VERSION
-node-pre-gyp build package --target=$NODE_VERSION --target_arch=ia32
