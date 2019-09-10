0587d2035a5805ce2fb671e7f833531c13490156
Thiago Lugli <thiago.lugli@ifood.com.br>
2019-09-10 14:35:08 -0300

    Trying to support older versions

--- src/macros.hh
+++ src/macros.hh
@@ -13,7 +13,6 @@
 #  define MY_NODE_MODULE_ISOLATE_PRE  isolate, 
 #  define MY_NODE_MODULE_ISOLATE_POST , isolate 
 #  define MY_NODE_MODULE_HANDLESCOPE MY_NODE_MODULE_ISOLATE_DECL v8::HandleScope scope(MY_NODE_MODULE_ISOLATE)
-#  define MY_NODE_MODULE_CALLBACK(name) void name(const Nan::FunctionCallbackInfo<v8::Value>& iArgs)
 #  define V8_VALUE_NEW(type, value)   v8::type::New(MY_NODE_MODULE_ISOLATE_PRE value)
 #  define V8_VALUE_NEW_DEFAULT(type)   v8::type::New(MY_NODE_MODULE_ISOLATE)
 #  define V8_STRING_NEW_UTF8(value)   v8::String::NewFromUtf8(MY_NODE_MODULE_ISOLATE_PRE value)
@@ -32,10 +31,9 @@
 #  define MY_NODE_MODULE_ISOLATE_PRE
 #  define MY_NODE_MODULE_ISOLATE_POST
 #  define MY_NODE_MODULE_HANDLESCOPE v8::HandleScope scope;
-#  define MY_NODE_MODULE_CALLBACK(name) void name(const Nan::FunctionCallbackInfo<v8::Value>& iArgs)
 #  define V8_VALUE_NEW(type, value)   v8::type::New(value)
 #  define V8_VALUE_NEW_DEFAULT(type)   v8::type::New()
-#  define V8_STRING_NEW_UTF8(value)   Nan::Utf8String::New(MY_NODE_MODULE_ISOLATE_PRE value)
+#  define V8_STRING_NEW_UTF8(value)   Nan::Utf8String(MY_NODE_MODULE_ISOLATE_PRE value)
 #  define V8_STRING_NEW_2BYTES(value)   v8::String::New(MY_NODE_MODULE_ISOLATE_PRE value)
 
 #  define RETURN_EXCEPTION(msg) return v8::ThrowException(v8::Exception::TypeError(msg)) 
@@ -45,21 +43,38 @@
 #  define MY_NODE_MODULE_RETURN_UNDEFINED()   return scope.Close(v8::Undefined())
 #endif
 
-#if NODE_VERSION_AT_LEAST(0, 11, 10) // for node-webkit v.0.9.2 which uses node v0.11.9
-#  define V8_VALUE_NEW_V_0_11_10(type, value)          v8::type::New(MY_NODE_MODULE_ISOLATE_PRE value)
-#  define V8_VALUE_NEW_DEFAULT_V_0_11_10(type)         v8::type::New(MY_NODE_MODULE_ISOLATE)
+#if NODE_VERSION_AT_LEAST(4, 0, 0)
+#define MY_MODULE_SET_METHOD(exports, name, method) Nan::SetMethod(exports, name, method)
+#define MY_NODE_MODULE_CALLBACK(name) void name(const Nan::FunctionCallbackInfo<v8::Value>& iArgs)
+#define V8_LOCAL_STRING_FROM_VALUE(value) value->ToString(Nan::GetCurrentContext()).FromMaybe(v8::Local<v8::String>())
+#define REQUIRE_ARGUMENT_INTEGER(args, i, var)                             \
+    int var;                                                                   \
+    if (args[i]->IsInt32()) {                                             \
+        var = args[i]->Int32Value(Nan::GetCurrentContext()).FromJust();                                           \
+    }                                                                          \
+    else {                                                                     \
+        RETURN_EXCEPTION_STR("Argument " #i " must be an integer");                 \
+    }
 #else
-#  define V8_VALUE_NEW_V_0_11_10(type, value)          v8::type::New(value)
-#  define V8_VALUE_NEW_DEFAULT_V_0_11_10(type)         v8::type::New()
+#define MY_MODULE_SET_METHOD(exports, name, method) NODE_SET_METHOD(exports, name, method)
+#define MY_NODE_MODULE_CALLBACK(name) void name(const v8::FunctionCallbackInfo<v8::Value>& iArgs)
+#define V8_LOCAL_STRING_FROM_VALUE(value) value->ToString()
+#define REQUIRE_ARGUMENT_INTEGER(args, i, var)                             \
+    int var;                                                                   \
+    if (args[i]->IsInt32()) {                                             \
+        var = args[i]->Int32Value();                                           \
+    }                                                                          \
+    else {                                                                     \
+        RETURN_EXCEPTION_STR("Argument " #i " must be an integer");                 \
+    }
 #endif
-
 
 #define V8_STR_CONC(left, right)                              \
 	v8::String::Concat(V8_STRING_NEW_UTF8(left), V8_STRING_NEW_UTF8(right))
 		
 #define REQUIRE_ARGUMENTS(args, n)                                                   \
     if (args.Length() < (n)) {                                                 \
-       RETURN_EXCEPTION_STR("Expected " #n " arguments");                       \
+        RETURN_EXCEPTION_STR("Expected " #n " arguments");                       \
     }
 
 
@@ -90,11 +105,11 @@
 
 #define REQUIRE_ARGUMENT_STRING(args, i, var)                                        \
     ARG_CHECK_STRING(args, i);                                                       \
-    Nan::Utf8String var(args[i]->ToString(Nan::GetCurrentContext()).FromMaybe(v8::Local<v8::String>())); \
+    Nan::Utf8String var(V8_LOCAL_STRING_FROM_VALUE(args[i])); \
 
 #define REQUIRE_ARGUMENT_STRINGW(args, i, var)                                        \
     ARG_CHECK_STRING(args, i);                                                       \
-    v8::Local<v8::String> var(args[i]->ToString(Nan::GetCurrentContext()).FromMaybe(v8::Local<v8::String>())); \
+    v8::Local<v8::String> var(V8_LOCAL_STRING_FROM_VALUE(args[i])); \
 
 
 #define OPTIONAL_ARGUMENT_FUNCTION(i, var)                                     \
@@ -106,15 +121,6 @@
         var = v8::Local<v8::Function>::Cast(args[i]);                                  \
     }
 
-
-#define REQUIRE_ARGUMENT_INTEGER(args, i, var)                             \
-    int var;                                                                   \
-    if (args[i]->IsInt32()) {                                             \
-        var = args[i]->Int32Value(Nan::GetCurrentContext()).FromJust();                                           \
-    }                                                                          \
-    else {                                                                     \
-        RETURN_EXCEPTION_STR("Argument " #i " must be an integer");                 \
-    }
 
 #define OPTIONAL_ARGUMENT_INTEGER(args, i, var, default)                             \
     int var;                                                                   \
